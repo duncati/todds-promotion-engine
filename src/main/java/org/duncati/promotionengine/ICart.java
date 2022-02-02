8d9288719249492a576619624b789e9b1863a128
Todd <tdelaune@gmail.com>
2022-02-02 21:18:06 +0100

    Implemented basic price and promotion stores Made Cart function correctly Basic test scenarios now pass

--- src/main/java/org/duncati/promotionengine/ICart.java
+++ src/main/java/org/duncati/promotionengine/ICart.java
@@ -7,5 +7,6 @@ public interface ICart {
     public void addItem(String sku);
     public void addItem(String sku, int quantity);
     public void removeItem(String sku);
+    public void removeItem(String sku, int quantity);
     public BigInteger getTotal();
 }
